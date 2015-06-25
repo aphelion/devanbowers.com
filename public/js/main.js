58f3ab35448c81c97f9a1ac9d552517fd09ce5cb
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-06-24 21:51:50 -0700

    cleanup css, hover image on uiux

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#uiux').hover(
+        function () {
+            this.src = 'images/cs_icon.png';
+        },
+        function () {
+            this.src = 'images/uiux_icon.png';
+        }
+    )
+});
