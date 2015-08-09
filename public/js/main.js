3c93b7a47c940d4606a533e3e19092f35de0c15b
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-08-09 12:39:38 -0700

    cleanup unused files

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,2 @@
 $(document).ready(function () {
-    $('#uiux').hover(
-        function () {
-            this.src = 'images/cs_icon.png';
-        },
-        function () {
-            this.src = 'images/uiux_icon.png';
-        }
-    )
 });
